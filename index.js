c285a4a0c5e9494b65823ada6cda3f7ca342d89f
Brian Fryer <brianfryer@gmail.com>
2013-07-27 13:36:04 -0500

    Fixed… well, everything.

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var app = express();
 
+(function() {
+  var five = require("johnny-five");
+  five.Board().on("ready", function() {
+    var theLED = new five.Led.RGB([ 9, 10, 11 ]);
+    this.repl.inject({
+      theLED: theLED
+    });
+  });
+})();
+
+var updateColor = function(hex) {
+  theLED.color(hex);
+};
+
 app.get('/', function(req, res) {
   var body = 'Give me a hex value!';
   res.end(body);
@@ -8,15 +22,7 @@ app.get('/', function(req, res) {
 
 app.get('/:hex', function(req, res) {
   var hex = req.params.hex;
-  var five = require("johnny-five");
-
-  five.Board().on("ready", function() {
-    var a = new five.Led.RGB([ 9, 10, 11 ]);
-    this.repl.inject({
-      a: a
-    });
-    a.color(hex);
-  });
+  updateColor(hex);
   res.end(hex);
 });
 
